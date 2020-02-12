1badb8417f17825523e3294cf8e62cc167296649
Georgy Filin <8740365@mail.ru>
2020-02-12 19:26:02 +0300

    Познакомился с понятием наследования!

--- Snake/Snake/VerticalLine.cs
+++ Snake/Snake/VerticalLine.cs
@@ -6,27 +6,18 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
-    class VerticalLine
-    {
-        List<Point> pList;
+    class VerticalLine : Figure
+    {     
 
         public VerticalLine(int yLeft, int yRight, int x, char sym)
         {
             pList = new List<Point>();
             for (int y = yLeft; y <= yRight; y++)
             {
-                Point p1 = new Point(x, y, sym);
-                pList.Add(p1);
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
             }
 
-        }
-
-        public void Drow()
-        {
-            foreach (Point p1 in pList)
-            {
-                p1.Draw();
-            }
-        }
+        }        
     }
 }
