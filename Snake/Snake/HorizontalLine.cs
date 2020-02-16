9681b4c06e766a8be46d003285ce663a802a8555
Georgy Filin <8740365@mail.ru>
2020-02-16 19:22:45 +0300

    Изучаем полиморфизм, препятствия.

--- Snake/Snake/HorizontalLine.cs
+++ Snake/Snake/HorizontalLine.cs
@@ -11,12 +11,19 @@ namespace Snake
         public HorizontalLine(int xLeft, int xRight, int y, char sym)
         {
             pList = new List<Point>();
-            for(int x = xLeft; x <= xRight; x++)
+            for (int x = xLeft; x <= xRight; x++)
             {
                 Point p = new Point(x, y, sym);
                 pList.Add(p);
             }
+        }
+            public override void Draw()
+            { 
+                Console.ForegroundColor = ConsoleColor.Yellow;
 
-        }        
+                base.Draw();
+
+                Console.ForegroundColor = ConsoleColor.White;
+            }            
     }
 }
