efe49b4bcfcf494e58561eb313dc2268ca9b62c7
Georgy Filin <8740365@mail.ru>
2020-02-16 18:39:01 +0300

    Проправил ошибку с Thread, змейка двигается и ест доллары = )

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Snake
 {
@@ -29,17 +29,31 @@ namespace Snake
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Drow();
 
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
             
 
             while(true)
             {
+                if (snake.Eat( food ))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+
+                Thread.Sleep(100);
+
                 if(Console.KeyAvailable)
                 {
                     ConsoleKeyInfo key = Console.ReadKey();
                     snake.HandleKey(key.Key);
-                }
-                Thread.Sleep(100);
-                snake.Move();
+                }                
             }
             
             
